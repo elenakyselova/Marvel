f9dbd8fc177df1e3e985ef1a748a7d5fa31b08ed
Olena Kyselova <ekyselov@gmail.com>
2023-04-13 18:43:59 +0300

    form

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -8,7 +8,11 @@ import Spinner from '../spinner/spinner';
 const Page404 = lazy(() => import('../pages/Page404'));
 const MainPage= lazy(() => import('../pages/MainPage'));
 const ComicsPage = lazy(() => import('../pages/ComicsPage'));
-const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
+
+
+const SingleComicLayout = lazy(() => import('../pages/singleComicLayout/SingleComicLayout'));
+const SingleCharacterLayout = lazy(() => import('../pages/singleCharacterLayout/SingleCharacterLayout'));
+const SinglePage = lazy(() => import('../pages/SinglePage'));
 
 const App = () => {
     
@@ -25,12 +29,12 @@ const App = () => {
                         <Route exact path="/comics">
                             <ComicsPage/>
                         </Route>
-                        <Route exact path="/comics/:comicId">
-                            <SingleComicPage/>
-                        </Route>
-                        <Route exact path="/comics/:comicId">
-                            <SingleComicPage/>
-                        </Route>
+                        <Route exact path="/comics/:id">
+                         <SinglePage Component={SingleComicLayout} dataType='comic'/>
+                            </Route>
+                         <Route exact path="/characters/:id">
+                                <SinglePage Component={SingleCharacterLayout} dataType='character'/>
+                            </Route>
                         <Route path="*">
                             <Page404/>
                              </Route>
