c4c8ee6817fce7f345341a3485bb5674d9596720
Olena Kyselova <ekyselov@gmail.com>
2023-04-13 10:28:16 +0300

    routers

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,39 +1,45 @@
-import { useState } from "react";
-import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
-import decoration from '../../resources/img/vision.png';
+import {lazy, Suspense } from 'react';
+
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import AppHeader from "../appHeader/AppHeader";
+import Spinner from '../spinner/spinner';
+
+const Page404 = lazy(() => import('../pages/Page404'));
+const MainPage= lazy(() => import('../pages/MainPage'));
+const ComicsPage = lazy(() => import('../pages/ComicsPage'));
+const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
 
 const App = () => {
     
-    const [selectedChar, setChar] = useState(null);
-
-    const onCharSelected = (id) => {
-        setChar(id);
-    }
-
     return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-                <ErrorBoundary>
-                    <RandomChar/>
-                </ErrorBoundary>
-                <div className="char__content">
-                    <ErrorBoundary>
-                        <CharList onCharSelected={onCharSelected}/>
-                    </ErrorBoundary>
-                    <ErrorBoundary>
-                        <CharInfo charId={selectedChar}/>
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
-            </main>
-        </div>
+        <Router>
+            <div className="app">
+                <AppHeader/>
+                <main>
+                   <Suspense fallback={<Spinner/>}>
+                        <Switch>
+                             <Route exact path="/">
+                            <MainPage/>
+                        </Route>
+                        <Route exact path="/comics">
+                            <ComicsPage/>
+                        </Route>
+                        <Route exact path="/comics/:comicId">
+                            <SingleComicPage/>
+                        </Route>
+                        <Route exact path="/comics/:comicId">
+                            <SingleComicPage/>
+                        </Route>
+                        <Route path="*">
+                            <Page404/>
+                             </Route>
+                         </Switch>
+                   </Suspense>
+                </main>
+            </div>
+        </Router>
     )
 }
 
-export default App;
+export default App; 
--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,8 +1,8 @@
 import {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
 
-import MarvelService from '../../services/MarvelService';
-import Spinner  from '../spinner/spinner';
+import useMarvelService from '../../services/MarvelService';
+import Spinner from '../spinner/spinner'; 
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import Skeleton from '../skeleton/Skeleton';
 
@@ -11,18 +11,12 @@ import './charInfo.scss';
 const CharInfo = (props) => {
 
     const [char, setChar] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
-}
 
-    const marvelService = new MarvelService();
+    const {loading, error, getCharacter, clearError} = useMarvelService();
 
     useEffect(() => {
         updateChar();
-    },[props.charId])
-   
-
-
+    }, [props.charId])
 
     const updateChar = () => {
         const {charId} = props;
@@ -30,42 +24,29 @@ const CharInfo = (props) => {
             return;
         }
 
-        onCharLoading();
-        marvelService.getCharacter(charId)
-            .then(this.onCharLoaded)
-            .catch(this.onError);
+        clearError();
+        getCharacter(charId)
+            .then(onCharLoaded)
     }
 
     const onCharLoaded = (char) => {
-            setChar(char);
-            setLoading(false);
+        setChar(char);
     }
 
-    const onCharLoading = () => {
-        setLoading(true);
-    }
+    const skeleton = char || loading || error ? null : <Skeleton/>;
+    const errorMessage = error ? <ErrorMessage/> : null;
+    const spinner = loading ? <Spinner/> : null;
+    const content = !(loading || error || !char) ? <View char={char}/> : null;
 
-    const onError = () => {
-            setLoading(false);
-            setError(true);
-    }
-
-
-        const skeleton = char || loading || error ? null : <Skeleton/>;
-        const errorMessage = error ? <ErrorMessage/> : null;
-        const spinner = loading ? <Spinner/> : null;
-        const content = !(loading || error || !char) ? <View char={char}/> : null;
-
-        return (
-            <div className="char__info">
-                {skeleton}
-                {errorMessage}
-                {spinner}
-                {content}
-            </div>
-        )
-    
-
+    return (
+        <div className="char__info">
+            {skeleton}
+            {errorMessage}
+            {spinner}
+            {content}
+        </div>
+    )
+}
 
 const View = ({char}) => {
     const {name, description, thumbnail, homepage, wiki, comics} = char;
